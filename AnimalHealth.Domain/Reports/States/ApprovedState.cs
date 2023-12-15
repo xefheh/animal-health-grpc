0febffeb94fdb5d2bf35686d304601e9ada1259e
NikitaVologin <kotgot47@gmail.com>
2023-12-15 10:24:46 +0500

    Расширение гитигнора

--- AnimalHealth.Domain/Reports/States/ApprovedState.cs
+++ AnimalHealth.Domain/Reports/States/ApprovedState.cs
@@ -2,33 +2,33 @@
 
 namespace AnimalHealth.Domain.Reports
 {
-    public class ApprovedState : IReportState
-    {
-        DateTime date;
-        public DateTime Date 
+        public class ApprovedState : IReportState
         {
-            get => date.ToUniversalTime();
-            set => date = value; 
+            DateTime date;
+            public DateTime Date 
+            {
+                get => date.ToUniversalTime();
+                set => date = value; 
+            }
+
+            public User Changer { get;  set; }
+
+            public string Name { get => "Одобрен"; }
+
+            public ApprovedState(DateTime date, User user) =>
+                (Date, Changer) = (date, user);
+
+            public ApprovedState() { }
+
+            public void Approve(Report report, DateTime date, User user)
+            {
+                return;
+            }
+
+            public void Cancel(Report report, DateTime date, User user) =>
+                report.State = new CreatedState(date, user);
+
+            public void Send(Report report, DateTime date, User user) =>
+                report.State = new SentState(date, user);
         }
-
-        public User Changer { get;  set; }
-
-        public string Name { get => "Одобрен"; }
-
-        public ApprovedState(DateTime date, User user) =>
-            (Date, Changer) = (date, user);
-
-        public ApprovedState() { }
-
-        public void Approve(Report report, DateTime date, User user)
-        {
-            return;
-        }
-
-        public void Cancel(Report report, DateTime date, User user) =>
-            report.State = new CreatedState(date, user);
-
-        public void Send(Report report, DateTime date, User user) =>
-            report.State = new SentState(date, user);
-    }
 }
