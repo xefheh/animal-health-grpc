46752a3e599bc8d82a71056cd16e6ca8ce49fe39
xefheh <114147391+xefheh@users.noreply.github.com>
2023-12-21 08:44:46 +0500

    Realized logger;

--- AnimalHealth.API/Services/OtherSourceService.cs
+++ AnimalHealth.API/Services/OtherSourceService.cs
@@ -1,6 +1,5 @@
 using AnimalHealth.Application.Interfaces.OtherSource;
 using AnimalHealth.Application.Models;
-using AnimalHealth.Application.OtherViews;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 
@@ -9,21 +8,53 @@ namespace AnimalHealth.API.Services;
 public class OtherSourceService : OtherResourceProto.OtherResourceProtoBase
 {
     private readonly IOtherSource _otherSource;
+    private readonly ILogger<OtherSourceService> _logger;
 
-    public OtherSourceService(IOtherSource otherSource) => _otherSource = otherSource;
+    public OtherSourceService(IOtherSource otherSource, ILogger<OtherSourceService> logger) =>
+        (_otherSource, _logger) = (otherSource, logger);
 
-    public override Task<AnimalModelList> GetAnimals(Empty request, ServerCallContext context) =>
-        _otherSource.GetAnimalsAsync(context.CancellationToken);
+    public override async Task<AnimalModelList> GetAnimals(Empty request, ServerCallContext context)
+    {
+        _logger.LogInformation($"[OTHER SOURCE] Invoked to get the animal list");
+        var animals = await _otherSource.GetAnimalsAsync(context.CancellationToken);
+        _logger.LogInformation("[OTHER SOURCE] Successfully. Animal List: {@List}; Count: {Count}",
+            animals.Animals, animals.Animals.Count());
+        return animals;
+    }
 
-    public override Task<DiseaseModelList> GetDiseases(Empty request, ServerCallContext context) =>
-        _otherSource.GetDiseasesAsync(context.CancellationToken);
+    public override async Task<DiseaseModelList> GetDiseases(Empty request, ServerCallContext context)
+    {
+        _logger.LogInformation($"[OTHER SOURCE] Invoked to get the disease list");
+        var diseases = await _otherSource.GetDiseasesAsync(context.CancellationToken);
+        _logger.LogInformation("[OTHER SOURCE] Successfully. Disease List: {@List}; Count: {Count}",
+            diseases.Diseases, diseases.Diseases.Count());
+        return diseases;
+    }
 
-    public override Task<VaccineModelList> GetVaccines(Empty request, ServerCallContext context) =>
-        _otherSource.GetVaccinesAsync(context.CancellationToken);
+    public override async Task<VaccineModelList> GetVaccines(Empty request, ServerCallContext context)
+    {
+        _logger.LogInformation($"[OTHER SOURCE] Invoked to get the vaccine list");
+        var vaccines = await _otherSource.GetVaccinesAsync(context.CancellationToken);
+        _logger.LogInformation("[OTHER SOURCE] Successfully. Vaccine List: {@List}; Count: {Count}",
+            vaccines.Vaccines, vaccines.Vaccines.Count());
+        return vaccines;
+    }
 
-    public override Task<LocalityModelList> GetLocalities(Empty request, ServerCallContext context) =>
-        _otherSource.GetLocalitiesAsync(context.CancellationToken);
+    public override async Task<LocalityModelList> GetLocalities(Empty request, ServerCallContext context)
+    {
+        _logger.LogInformation($"[OTHER SOURCE] Invoked to get the locality list");
+        var localities = await _otherSource.GetLocalitiesAsync(context.CancellationToken);
+        _logger.LogInformation("[OTHER SOURCE] Successfully. Locality List: {@List}; Count: {Count}",
+            localities.Localities, localities.Localities.Count());
+        return localities;
+    }
 
-    public override Task<UserModelList> GetUsers(Empty request, ServerCallContext context) =>
-        _otherSource.GetUsersAsync(context.CancellationToken);
+    public override async Task<UserModelList> GetUsers(Empty request, ServerCallContext context)
+    {
+        _logger.LogInformation($"[OTHER SOURCE] Invoked to get the users list");
+        var users = await _otherSource.GetUsersAsync(context.CancellationToken);
+        _logger.LogInformation("[OTHER SOURCE] Successfully. User List: {@List}; Count: {Count}",
+            users.Users, users.Users.Count());
+        return users;
+    }
 }
